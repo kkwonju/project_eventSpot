ee318ce2ba16696c886b241ff1466845ce6320e6
kkwonju <kkwo4140@gmail.com>
2023-05-05 23:37:23 +0900

    작업 0, 개발환경 및 실행환경 세팅

---
+++ src/main/java/com/kkwo/demo/KkwoEventSpotApplication.java
@@ -0,0 +1,13 @@
+package com.kkwo.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KkwoEventSpotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KkwoEventSpotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kkwo/demo/KkwoEventSpotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kkwo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KkwoEventSpotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
